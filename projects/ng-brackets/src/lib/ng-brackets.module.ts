0fd49d62ff4a75e40e6ffcedc2b4bcaa1088c4e5
Javier Marín <contacto@ideatic.net>
2022-11-22 08:41:32 +0100

    refactor: remove use of route-dynamic component refactor: remove use of RouterModule.forChild()

--- projects/ng-brackets/src/lib/ng-brackets.module.ts
+++ projects/ng-brackets/src/lib/ng-brackets.module.ts
@@ -2,6 +2,7 @@ import {NgModule} from '@angular/core';
 import {NgBracketsComponent} from './components/ng-brackets.component';
 import {CommonModule} from '@angular/common';
 
+/** @deprecated */
 @NgModule({
   declarations: [NgBracketsComponent],
   imports: [CommonModule],
