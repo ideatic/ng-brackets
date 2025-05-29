91a0f9380faeb141e42e95be625613ac63e43dfc
Javier Marín <contacto@ideatic.net>
2025-05-29 16:56:23 +0200

    refator: use new Angular 20 features

--- projects/ng-brackets/src/lib/components/ng-brackets.component.ts
+++ projects/ng-brackets/src/lib/components/ng-brackets.component.ts
@@ -1,5 +1,12 @@
-import {NgTemplateOutlet} from "@angular/common";
-import {ChangeDetectionStrategy, Component, contentChild, HostBinding, TemplateRef, input} from '@angular/core';
+import { NgTemplateOutlet } from '@angular/common';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  contentChild,
+  HostBinding,
+  input,
+  TemplateRef
+} from '@angular/core';
 
 export interface NgBracketsRound {
   name: string;
@@ -9,36 +16,46 @@ export interface NgBracketsRound {
 
 export interface NgBracketsFixture {
   caption: string;
-  home: { name: string, score: number };
-  away: { name: string, score: number };
+  home: { name: string; score: number };
+  away: { name: string; score: number };
 }
 
 @Component({
-    selector: 'ng-brackets',
-    changeDetection: ChangeDetectionStrategy.OnPush,
-    imports: [NgTemplateOutlet],
-    templateUrl: './ng-brackets.component.html',
-    styleUrl: './ng-brackets.component.less'
+  selector: 'ng-brackets',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  imports: [NgTemplateOutlet],
+  templateUrl: './ng-brackets.component.html',
+  styleUrl: './ng-brackets.component.less'
 })
 export class NgBracketsComponent {
-  public readonly rounds = input<NgBracketsRound[]>(undefined);
+  public readonly rounds = input<NgBracketsRound[]>();
   public readonly rounded = input(true);
   public readonly mode = input<'brackets' | 'list'>('brackets');
-  public readonly fixtureHeight = input<number>(undefined);
+  public readonly fixtureHeight = input<number>();
 
-  protected readonly fixtureTemplate = contentChild.required('fixtureTemplate', {read: TemplateRef});
-  protected readonly roundTitleTemplate = contentChild.required('roundTitleTemplate', {read: TemplateRef});
+  protected readonly fixtureTemplate = contentChild.required(
+    'fixtureTemplate',
+    { read: TemplateRef }
+  );
+  protected readonly roundTitleTemplate = contentChild.required(
+    'roundTitleTemplate',
+    { read: TemplateRef }
+  );
 
   @HostBinding('class')
   private get _classes(): string {
     return (this.rounded() ? 'rounded ' : '') + this.mode();
   }
 
-  protected isStraightLine(roundIndex: number) { // Straight line when next round has the same number of fixtures as the current one
+  protected isStraightLine(roundIndex: number) {
+    // Straight line when next round has the same number of fixtures as the current one
     const currentRound = this.rounds()[roundIndex];
-    return currentRound
-      && this.rounds()[roundIndex + 1]
-      && currentRound.fixtures.length <= this.rounds()[roundIndex + 1].fixtures.length;
+    return (
+      currentRound &&
+      this.rounds()[roundIndex + 1] &&
+      currentRound.fixtures.length <=
+        this.rounds()[roundIndex + 1].fixtures.length
+    );
   }
 
   protected calculateRoundHeightFactor(round: NgBracketsRound): number {
